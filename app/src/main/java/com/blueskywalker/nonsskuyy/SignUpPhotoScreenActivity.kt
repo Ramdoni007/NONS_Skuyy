15b0c512f2dca4232a1b67f84fc59e23f6dca328
Ramdoni007 <ramd42156@gmail.com>
2020-09-17 15:06:42 +0700

    Penambahan Sign Up Photo Page :)

---
+++ app/src/main/java/com/blueskywalker/nonsskuyy/SignUpPhotoScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.blueskywalker.nonsskuyy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpPhotoScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_photo_screen)
+    }
+}
