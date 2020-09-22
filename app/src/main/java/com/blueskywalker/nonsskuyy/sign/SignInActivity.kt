52309f5b25436942a9d2687ad204abd8f7beca2f
Downy21 <cikinangka007@gmail.com>
2020-09-22 15:42:25 +0700

    Materi Kelas BWAMov Checkout Page Belom kelar :)

--- app/src/main/java/com/blueskywalker/nonsskuyy/sign/SignInActivity.kt
+++ app/src/main/java/com/blueskywalker/nonsskuyy/sign/SignInActivity.kt
@@ -1,7 +1,8 @@
-package com.blueskywalker.nonsskuyy
+package com.blueskywalker.nonsskuyy.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blueskywalker.nonsskuyy.R
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
