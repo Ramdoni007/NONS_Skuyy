cdf865e638993235a0a48e665a80230bfb7fe0ad
Downy21 <cikinangka007@gmail.com>
2020-09-22 08:09:37 +0700

    Pilih Bangku Page Kelar :) Noted : Tinggal lengkapi bangku  B C D dan seterusnya

---
+++ app/src/main/java/com/blueskywalker/nonsskuyy/PilihBangkuActivity.kt
@@ -0,0 +1,11 @@
+package com.blueskywalker.nonsskuyy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PilihBangkuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pilih_bangku)
+    }
+}
