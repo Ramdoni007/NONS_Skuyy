0336b916ddb5b5757148008e31bca4af842976e8
Ramdoni007 <ramd42156@gmail.com>
2020-09-25 16:32:06 +0700

    Materi Kelas BWAMov Kelar: penambahan Firebase dengan Project NonsKuyyy :)

--- app/src/main/java/com/blueskywalker/nonsskuyy/sign/SignInActivity.kt
+++ app/src/main/java/com/blueskywalker/nonsskuyy/sign/SignInActivity.kt
@@ -1,13 +1,28 @@
 package com.blueskywalker.nonsskuyy.sign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.blueskywalker.nonsskuyy.R
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        btn_home.setOnClickListener {
+            // Write a message to the database
+
+            // Write a message to the database
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("message")
+
+            myRef.setValue("Hello, World!")
+        }
+
+
     }
 }
 
